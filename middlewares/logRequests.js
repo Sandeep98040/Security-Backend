ce9330e0c877bbd7c976e0b5ddda02395b9bd7df
Sandeep98040 <baralsandeep50@gmail.com>
2024-08-17 14:10:51 +0545

    log request

---
+++ middlewares/logRequests.js
@@ -0,0 +1,28 @@
+// middlewares/logRequest.js
+const logger = require('../utilities/logger');
+const Log = require('../models/logs');
+
+const logRequest = async (req, res, next) => {
+    if (req.user) {
+        try {
+            // Log to the database using Mongoose
+            const logEntry = new Log({
+                username: req.user.email,
+                url: req.originalUrl,
+                method: req.method,
+                role: req.user.role,
+                status: 'unknown',
+                time: new Date()
+            });
+            await logEntry.save();
+
+            // Log to Winston (console, file, etc.)
+            logger.info(`User: ${req.user.email}, Role: ${req.user.role}, URL: ${req.originalUrl}, Method: ${req.method}`);
+        } catch (error) {
+            logger.error(`Logging error: ${error.message}`); // Log errors to Winston
+        }
+    }
+    next();
+};
+
+module.exports = logRequest;
