9f8cec3887fbd9326551efce1d9e9e3fc8cfee3f
Sandeep98040 <baralsandeep50@gmail.com>
2024-08-17 14:12:59 +0545

    user route

---
+++ routes/user_routes.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/user_controller');
+const { verifyUser } = require('../middlewares/auth');
+const logRequest = require('../middlewares/logRequests');  // Import the logging middleware
+
+router.route('/register')
+    .get((req, res, next) => res.status(405).json({ error: "GET method is not allowed" }))
+    .delete((req, res, next) => res.status(405).json({ error: "DELETE method is not allowed" }))
+    .put((req, res, next) => res.status(405).json({ error: "PUT method is not allowed" }))
+    .post(logRequest, userController.userRegister);
+
+router.route('/login')
+    .get((req, res, next) => res.status(405).json({ error: "GET method is not allowed" }))
+    .delete((req, res, next) => res.status(405).json({ error: "DELETE method is not allowed" }))
+    .put((req, res, next) => res.status(405).json({ error: "PUT method is not allowed" }))
+    .post(logRequest, userController.userLogin);
+
+router.route('/lockAccount')
+    .get((req, res, next) => res.status(405).json({ error: "GET method is not allowed" }))
+    .delete((req, res, next) => res.status(405).json({ error: "DELETE method is not allowed" }))
+    .put((req, res, next) => res.status(405).json({ error: "PUT method is not allowed" }))
+    .post(logRequest, userController.userAccountLock);
+
+router.route('/')
+    .post((req, res, next) => res.status(405).json({ error: "POST method is not allowed" }))
+    .get(logRequest, verifyUser, userController.getProfile)
+    .delete(logRequest, verifyUser, userController.deleteAccount)
+    .put(logRequest, verifyUser, userController.updateProfile);
+
+router.get('/passwordNeedChange', logRequest, verifyUser, userController.getPasswordExpiry);
+router.put('/changePassword', logRequest, verifyUser, userController.changePassword);
+
+router.post("/forgot-password", logRequest, userController.forgotPassword);
+router.put("/password/reset/:token", logRequest, userController.resetPassword);
+
+module.exports = router;
