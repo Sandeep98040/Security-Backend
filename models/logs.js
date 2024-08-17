d9aad988dd3744b2cc9485cae189196b2fa61538
Sandeep98040 <baralsandeep50@gmail.com>
2024-08-17 14:11:15 +0545

    model log

---
+++ models/logs.js
@@ -0,0 +1,15 @@
+
+const mongoose = require('mongoose');
+
+const logSchema = new mongoose.Schema({
+    username: String,
+    url: String,
+    method: String,
+    role: String,
+    status: String,
+    time: Date
+}, { timestamps: true });
+
+const Log = mongoose.model('Log', logSchema);
+
+module.exports = Log;
